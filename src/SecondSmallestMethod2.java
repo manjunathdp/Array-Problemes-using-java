72205d26fec7192e74532b62bfc542072e7a7589
Manjunath <manjunathdtc3@gmail.com>
2024-08-09 01:32:54 +0530

    Second-Smallest element in an array elements duplicate inputs not supported

---
+++ src/SecondSmallestMethod2.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class SecondSmallestMethod2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Array size: ");
+        int size = sc.nextInt();
+        int[] array = new int[size];
+        System.out.println("Enter "+size+" Array Elements");
+        for (int i = 0; i <  size; i++) {
+            array[i]= sc.nextInt();
+        }
+        sc.close();
+        int temp;
+        for (int i = 0; i < size; i++) {
+            for (int j = i+1; j < size ; j++) {
+                if (array[j]<array[i]) {
+                    temp = array[i];
+                    array[i]=array[j];
+                    array[j]=temp;
+                }
+            }
+            if (i==1){
+                break;
+            }
+        }
+        System.out.println("The Second Smallest Element in an Array is: "+array[1]);
+    }
+}
