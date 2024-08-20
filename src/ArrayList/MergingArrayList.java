921dd4a5cf7ee60398e0c70fb91e04b7deabafc0
Manjunath Pujar <manjunathdtc3@gmail.com>
2024-08-20 15:56:36 +0530

    Merging Two ArrayLists in Java

---
+++ src/ArrayList/MergingArrayList.java
@@ -0,0 +1,27 @@
+package ArrayList;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class MergingArrayList {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        ArrayList<Integer> a1 = new ArrayList<>();
+        ArrayList<Integer> a2 = new ArrayList<>();
+        ArrayList<Integer> merged= new ArrayList<>();
+        System.out.println("Enter 5 Elements for arrayList 1: ");
+        for (int i = 0; i < 5; i++) {
+            a1.add(sc.nextInt());
+        }
+        System.out.println("Enter 5 Elements for arrayList 2: ");
+        for (int i = 0; i < 5; i++) {
+            a2.add(sc.nextInt());
+        }
+        System.out.print("Displaying Merged list: ");
+        merged.addAll(a1);
+        merged.addAll(a2);
+        for (int item : merged) {
+            System.out.print(item+" ");
+        }
+    }
+}
